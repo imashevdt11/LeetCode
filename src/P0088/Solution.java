1e7bec7ca87e375092b5de394ed0dd8c3ebbf584
imashevdt11 <imashevdt@gmail.com>
2024-02-02 10:18:23 +0600

    February 2nd

---
+++ src/P0088/Solution.java
@@ -0,0 +1,16 @@
+package P0088;
+
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n != 0) {
+            int j = 0;
+            for (int i = m; i < nums1.length; i++) {
+                nums1[i] = nums2[j];
+                j++;
+            }
+            Arrays.sort(nums1);
+        }
+    }
+}
